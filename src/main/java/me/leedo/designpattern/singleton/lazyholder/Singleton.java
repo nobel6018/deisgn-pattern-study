482e7ab7b7be50a2d26eb017cc96a2510ce113b9
Lee, YoungHoon <nobel6018@gmail.com>
2022-03-15 11:42:09 +0900

    feat: singleton pattern

---
+++ src/main/java/me/leedo/designpattern/singleton/lazyholder/Singleton.java
@@ -0,0 +1,15 @@
+package me.leedo.designpattern.singleton.lazyholder;
+
+public class Singleton {
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+
+    private static class LazyHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+}

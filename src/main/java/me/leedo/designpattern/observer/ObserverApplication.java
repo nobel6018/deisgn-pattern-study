6d02d35a7684dcb3fda642189b02e61103ffd09a
Lee, YoungHoon <nobel6018@gmail.com>
2022-03-24 15:35:35 +0900

    update: example idea

--- src/main/java/me/leedo/designpattern/observer/ObserverApplication.java
+++ src/main/java/me/leedo/designpattern/observer/ObserverApplication.java
@@ -1,5 +1,6 @@
 package me.leedo.designpattern.observer;
 
+// 기상청 예제 -> 기상청에서 날씨 변화 공지 -> 어플에서 변화 감지
 public class ObserverApplication {
     public static void main(String[] args) {
         Cat cat = new Cat();

1a48b8c0eb9d7aa58f063067adb95332e899c833
Lee, YoungHoon <nobel6018@gmail.com>
2022-04-12 19:23:58 +0900

    update: student table class has student list

--- src/main/java/me/leedo/designpattern/proxy2/Student.java
+++ src/main/java/me/leedo/designpattern/proxy2/Student.java
@@ -1,13 +1,14 @@
 package me.leedo.designpattern.proxy2;
 
 public class Student {
-    private Long id;
-    private String name;
-    private School school;
+    private final Long id;
+    private final String name;
+    private final Long schoolId;
 
-    public Student(Long id, String name) {
+    public Student(Long id, String name, Long schoolId) {
         this.id = id;
         this.name = name;
+        this.schoolId = schoolId;
     }
 
     @Override
@@ -16,5 +17,9 @@ public class Student {
             "id=" + id +
             ", name='" + name + '\'' +
             '}';
+    }
+
+    public Long getSchoolId() {
+        return schoolId;
     }
 }
--- src/main/java/me/leedo/designpattern/proxy2/StudentTable.java
+++ src/main/java/me/leedo/designpattern/proxy2/StudentTable.java
@@ -1,23 +1,20 @@
 package me.leedo.designpattern.proxy2;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class StudentTable {
 
+    List<Student> students = List.of(
+        new Student(1L, "1번 학생", 1L),
+        new Student(2L, "2번 학생", 1L),
+        new Student(3L, "3번 학생", 1L),
+        new Student(4L, "4번 학생", 2L),
+        new Student(5L, "5번 학생", 2L)
+    );
+
     public List<Student> findBySchoolId(Long schoolId) {
-        if (schoolId == 1) {
-            return List.of(
-                new Student(1L, "1번 학생"),
-                new Student(2L, "2번 학생"),
-                new Student(3L, "3번 학생")
-            );
-        } else if (schoolId == 2) {
-            return List.of(
-                new Student(4L, "4번 학생"),
-                new Student(5L, "5번 학생")
-            );
-        } else {
-            throw new RuntimeException("해당하는 학교가 존재하지 않습니다");
-        }
+        return students.stream().filter(it -> Objects.equals(it.getSchoolId(), schoolId)).collect(Collectors.toList());
     }
 }
